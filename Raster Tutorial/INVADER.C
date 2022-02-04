92a49e437408784656e3502c7515e1560d589200
SorenEdwards <43620682+SorenEdwards@users.noreply.github.com>
2022-02-04 00:51:59 -0700

    Writing many little people to screen works.
    Works based on the assumptions of question 5, however, needs to be reworked to function under the condition that the assumptions of question 5 no longer hold.

---
+++ Raster Tutorial/INVADER.C
@@ -0,0 +1,89 @@
+<<<<<<< HEAD
+#include <stdio.h>      /* header needed for printfs and scanfs */
+#include <osbind.h>     /* header needed for Bconstat and Bconin */
+
+#define INVADER_HEIGHT 16
+
+typedef unsigned int UINT16;
+
+void plot_bitmap_16(UINT16*, int, int, UINT16*, UINT16);
+
+const UINT16 invader_bitmap[INVADER_HEIGHT] = {0x0000,
+										0x0810,
+										0x0810,
+										0x0420,
+										0x0240,
+										0x1FF8,
+										0x2004,
+										0x4662,
+										0x4002,
+										0x43C2,
+										0x2424,
+										0x1008,
+										0x0FF0,
+										0x0240,
+										0x0E70,
+										0x0000 };
+int main() {
+
+	
+	UINT16 * FB = (UINT16 *) Physbase();
+	UINT16 * FB_two = FB;
+	int x_two;
+	int x = 12;
+	int y = 0;
+	int f;
+	int j;
+	char ch;
+	plot_bitmap_16(FB, x,y, invader_bitmap, (UINT16) INVADER_HEIGHT);
+	getchar(ch);
+	x_two = 22;
+	plot_bitmap_16(FB_two, x_two, y, invader_bitmap, (UINT16) INVADER_HEIGHT);
+	getchar(ch);
+
+	return 0;
+}
+
+void plot_bitmap_16(UINT16 * base, int x, int y, const UINT16 * bitmap, UINT16 height) {
+	UINT16 offset = (40 * y) + (x >> 4);
+	int i;
+	for (i = 0; i < height; i++) {
+		*(base + offset + 40 * i) = invader_bitmap[i];
+	}
+}
+=======
+#define INVADER_HEIGHT 16
+
+int invader_bitmap[INVADER_HEIGHT] =
+{
+	0x0000,
+	0x0810,
+	0x0810,
+	0x0420,
+	0x0240,
+	0x1FF8,
+	0x2004,
+	0x4662,
+	0x4002,
+	0x43C2,
+	0x2424,
+	0x1008,
+	0x0FF0,
+	0x0240,
+	0x0E70,
+	0x0000
+};
+
+#UINT16 *FB = (UINT16 *) PhysBase();
+plot_bitmap(FB, 0,0, *invader_bitmap, INVADER_HEIGHT)
+
+void plot_bitmap_grid(U)
+
+void plot_bitmap_16(UINT16 *base, int x, int y, const UINT16 *bitmap,
+			unsigned int height) {
+	offset = (40 * y) + (x >> 4);
+	for (int i = 0; i < height; i++) {
+		*(base + offset + 40 * i) = invader_bitmap[i];
+}
+	
+>>>>>>> 7957dad0294bd51848473e0f9c4ab46b9ec73926
